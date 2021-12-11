d658f5e4498e2b856495efc4da5f8cb90bdba178
Mostafa Mahmoud <mmzaghlool52@gmail.com>
2021-12-11 21:13:45 +0200

    home page

--- src/controller/HomeController.ts
+++ src/controller/HomeController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
+import contacts from '../data/contact.json';
+import profile from '../data/profile.json';
 
 export default class {
     static async render(req: Request, res: Response) {
-        res.render('home/index');
+        res.render('home/index', { profile, contacts });
     }
 }
