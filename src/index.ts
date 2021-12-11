6ccb7197f3737bec5af10ce87dd732450dc70104
Mostafa Mahmoud <mmzaghlool52@gmail.com>
2021-12-11 18:57:52 +0200

    404 and nvbar

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,30 @@ import express, { Request, Response } from 'express';
 import { middleWares } from 'nodejs-express-utils';
 import cors from 'cors';
 import routers from './routes';
+import morgan from 'morgan';
 
 const app = express();
 
 // view engine setup
 app.use(cors());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+// register view engine
+app.set('view engine', 'ejs');
+
+// static files
+app.use(express.static('public'));
+
+// XSS Filter
 app.use(middleWares.xssFilter());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.get('/', (_, res) => res.send('<p>Health check</p>'));
-app.use('/v1', routers);
+// Morgan logger
+app.use(morgan('dev'));
 
-app.all('*', (_: Request, res: Response) => res.status(404).json({ success: false, code: 'NOT_FOUND' }));
+// app.get('/', (_, res) => res.send('<p>Health check</p>'));
+app.use(routers);
+
+app.use((_: Request, res: Response) => res.redirect('/404'));
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 // import admin from './admin';;
 
 const router = express();
 
 // router.use('/admin', admin);
+
+router.get('/404', (_: Request, res: Response) => res.render('404', {title: 'Page Not Found'}));
 
 export default router;
